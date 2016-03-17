00980cda1e7aa51be6d5907548243150b7bcb32c
gibso124 <gibso124@msu.edu>
2016-03-17 14:42:48 -0400

    Updated code to account for duplicate vac samples (and one floral observation) that had to be removed. The code works, but the merged file is still imperfect.  The floral pairs to the removed vac samples still need to be removed.

--- NaturalEnemyAnalysis.R
+++ NaturalEnemyAnalysis.R
@@ -133,7 +133,7 @@ summary(floral.area)
 floral.data<-read.csv("peakbloomdatawithcontrols_DG_20160315.csv",header=TRUE)
 
 #Bring in Vacuum data
-vac.data<-read.csv("VAc_Data_2015_JK_03142016.csv",header=TRUE)
+vac.data<-read.csv("VAc_Data_2015_DG_03172016.csv",header=TRUE)
 
 #***Corrections and standardizations***
 
@@ -143,38 +143,79 @@ floral.area$mean_area<-NULL
 #Calculate new mean area in 'floral.area'
 floral.area$avgarea<-rowMeans(floral.area[,5:9])
 
+#Remove site from floral.area
+floral.area$site<-NULL
+
+#There are multiple cases in which there are two different samples with the same identifier. I cannot verify which is correct or
+#what the identity of the other is, so I must remove the whole pair.
+#this subsetting function also removes "NA" observations
+#25SWMREC3CONTROL
+#26SWMREC2COLA5
+#30CRC2CONTROL
+#37SWMREC4CONTROL
+#38NWMHRC1HEST
+#28_CRC_4_MOWED
+vac.corrected<-vac.data[which(vac.data$sample !="127" & vac.data$sample !="105" &
+                              vac.data$sample !="78" & vac.data$sample !="79" &
+                              vac.data$sample !="768" & vac.data$sample !="773" &
+                              vac.data$sample !="409" & vac.data$sample !="412" &
+                              vac.data$sample !="1567" & vac.data$sample !="1569" &
+                              vac.data$sample !="727" & vac.data$sample !="725"),]
+
+#8/4/15 4 CESTM (32SWMREC4CESTM) was accidentally measured twice. Delete second measurement
+floral.corrected<-floral.data[which(floral.data$new_record !="1139"),]
+summary(floral.data)
+summary(floral.corrected)
+
 #I know that the dates of floral data do not always match the dates of vac data (multiple sampling days)
 #extract month of year. Week is very useful for a phenology type analyses
 #because you don't have to deal with day-of-month numbers starting over 
 #in the middle of a phenological event.
 #Use Lubridate package to convert dates to week of year.
 library(lubridate)
-#format original dates in a new column in ISO format
-floral.data$newdate<-mdy(floral.data$date)
-floral.data$week<-week(floral.data$newdate)
-vac.data$newdate<-mdy(vac.data$date)
-vac.data$week<-week(vac.data$newdate)
+#format original dates in a new column in ISO format.
+floral.corrected$floraldate<-mdy(floral.corrected$date)
+floral.corrected$date<-NULL
+is.numeric(vac.corrected$sample)
+vac.corrected$vacdate<-mdy(vac.corrected$date)
+vac.data$date<-NULL
+
+#Lubridate weeks start on the first day of the year. ISOweek week one starts on the first sunday of the year.
+library(ISOweek)
+floral.corrected$isoweek<-isoweek(floral.corrected$floraldate)
+vac.corrected$isoweek<-isoweek(vac.corrected$vacdate)
+
 
 #Control titles are insconsistent. vac.data -- 'MOWED' 'CONTROL' floral.data -- 'mowed' 'control'.  Fix to all CAPS.
 #Fixed CAPS problem in floral.data in the 'species' column
-floral.data$species1<-gsub('control','CONTROL',floral.data$species)
-floral.data$species2<-gsub('mowed','MOWED',floral.data$species1)
+floral.corrected$species1<-gsub('control','CONTROL',floral.corrected$species)
+floral.corrected$species2<-gsub('mowed','MOWED',floral.corrected$species1)
+floral.corrected$species<-NULL
+floral.corrected$species1<-NULL
 
 #Rename 'species' to 'species2' in vac.data
-vac.data$species2<-vac.data$species
+vac.corrected$species2<-vac.corrected$species
+vac.corrected$species<-NULL
 
-#Concatenate week, site, block, species2 in 'floral.data' to create a single variable that uniquely identifies each sample.
+#Rename 'species' to 'species2' in floral.area
+floral.area$species2<-floral.area$species
+floral.area$species<-NULL
+
+#Concatenate ISOweek, site, block, species2 in 'floral.data' to create a single variable that uniquely identifies each sample.
 #This column will then be created in the vac.data file so that the two files can be merged.
-floral.data$sample_ident<-paste(floral.data$week,floral.data$site,floral.data$block,floral.data$species2,sep='')
+floral.corrected$sample_ident<-paste(floral.corrected$isoweek,floral.corrected$site,floral.corrected$block,floral.corrected$species2,sep='_')
 
 #Concatenate week, site, block, species2 in 'vac.data'
-vac.data$sample_ident<-paste(vac.data$week,vac.data$site,vac.data$block,vac.data$species2,sep='')
+vac.corrected$sample_ident<-paste(vac.corrected$isoweek,vac.corrected$site,vac.corrected$block,vac.corrected$species2,sep='_')
 
 #***Merging Files****
 
 #Merge floral.area into floral.data under new data frame 'floral.data.area'
-floral.data.area<-merge(floral.data,floral.area,by="species",all.x=TRUE)
+floral.data.area<-merge(floral.corrected,floral.area,by=c("species2"),all.x=TRUE)
 
 #Merge vac.data into floral.data.area to create new data frame "all.data"
-all.data<-merge(floral.data.area,vac.data,by="sample_ident",all.x=TRUE)
+all.data<-merge(floral.data.area,vac.corrected,by=c("sample_ident","site","block","species2","isoweek"),all.x=TRUE)
 summary(all.data)
+
+#Export Data
+write.csv(all.data, 'Merged_Data2.csv')
